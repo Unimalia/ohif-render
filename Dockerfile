827438445a98c8a9df4b1144da85e6e22a6e06eb
Unimalia <valentinomct@gmail.com>
2026-04-03 14:51:50 +0200

    Set OHIF viewer to Unimalia Orthanc datasource

--- Dockerfile
+++ Dockerfile
@@ -3,5 +3,4 @@ FROM ohif/app:v3.9.2
 USER root
 
 COPY app-config.js /usr/share/nginx/html/app-config.js
-
 RUN sed -i 's|</head>|<script src="/app-config.js"></script></head>|g' /usr/share/nginx/html/index.html
--- app-config.js
+++ app-config.js
@@ -1,6 +1,21 @@
 window.config = {
   routerBasename: '/',
+  extensions: [],
+  modes: [],
+  customizationService: {},
   showStudyList: true,
+  maxNumberOfWebWorkers: 3,
+  showWarningMessageForCrossOrigin: true,
+  showCPUFallbackMessage: true,
+  showLoadingIndicator: true,
+  experimentalStudyBrowserSort: false,
+  strictZSpacingForVolumeViewport: true,
+  groupEnabledModesFirst: true,
+  maxNumRequests: {
+    interaction: 100,
+    thumbnail: 75,
+    prefetch: 25,
+  },
   defaultDataSourceName: 'unimalia',
   dataSources: [
     {
@@ -8,15 +23,30 @@ window.config = {
       sourceName: 'unimalia',
       configuration: {
         friendlyName: 'UNIMALIA Orthanc',
+        name: 'unimalia',
+        wadoUriRoot: 'https://unimalia-imaging.onrender.com/orthanc/wado',
         qidoRoot: 'https://unimalia-imaging.onrender.com/orthanc/dicom-web',
         wadoRoot: 'https://unimalia-imaging.onrender.com/orthanc/dicom-web',
-        wadoUriRoot: 'https://unimalia-imaging.onrender.com/orthanc/wado',
         qidoSupportsIncludeField: true,
         supportsReject: false,
         supportsStow: false,
         imageRendering: 'wadors',
         thumbnailRendering: 'wadors',
+        enableStudyLazyLoad: true,
+        supportsFuzzyMatching: false,
+        supportsWildcard: false,
+        staticWado: false,
+        singlepart: 'bulkdata,video',
+        bulkDataURI: {
+          enabled: true,
+          relativeResolution: 'studies',
+        },
+        omitQuotationForMultipartRequest: true,
       },
     },
   ],
+  httpErrorHandler: error => {
+    console.warn(error.status);
+    console.warn(error);
+  },
 };
